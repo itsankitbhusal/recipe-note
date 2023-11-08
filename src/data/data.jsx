4652825fda5eaf13a25277fc2f10d10dade93c3c
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-08 17:25:49 +0545

    data source

---
+++ src/data/data.jsx
@@ -0,0 +1,46 @@
+export default[
+  {
+    "id": 1,
+    "title": "Spaghetti Carbonara",
+    "ingredients": [
+      "200g spaghetti",
+      "100g pancetta, diced",
+      "2 large eggs",
+      "50g Pecorino cheese, grated",
+      "Salt and black pepper to taste"
+    ],
+    "description": "Spaghetti Carbonara is a classic Roman pasta dish known for its rich and creamy sauce made with eggs, cheese, pancetta, and black pepper. It's quick, easy, and incredibly delicious.",
+    "image": "spaghetti_carbonara.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Chicken Alfredo Pasta",
+    "ingredients": [
+      "250g fettuccine pasta",
+      "2 boneless, skinless chicken breasts, thinly sliced",
+      "2 cups heavy cream",
+      "1/2 cup grated Parmesan cheese",
+      "2 cloves garlic, minced",
+      "Salt and white pepper to taste",
+      "Fresh parsley, chopped, for garnish"
+    ],
+    "description": "Chicken Alfredo Pasta is a creamy Italian pasta dish made with fettuccine noodles, tender chicken slices, and a rich Alfredo sauce. It's a comforting and satisfying meal that everyone loves.",
+    "image": "chicken_alfredo_pasta.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Chocolate Chip Cookies",
+    "ingredients": [
+      "1 cup unsalted butter, softened",
+      "1 cup granulated sugar",
+      "2 large eggs",
+      "1 teaspoon vanilla extract",
+      "2 cups all-purpose flour",
+      "1/2 teaspoon baking soda",
+      "1/2 teaspoon salt",
+      "2 cups semisweet chocolate chips"
+    ],
+    "description": "Chocolate Chip Cookies are classic homemade treats loved by people of all ages. They are soft, chewy, and packed with gooey chocolate chips. Perfect for satisfying your sweet cravings.",
+    "image": "chocolate_chip_cookies.jpg"
+  }
+]
