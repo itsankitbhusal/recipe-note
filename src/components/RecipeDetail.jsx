87dfdb39774af77870fb085c78aaadf36111f89e
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-09 17:12:17 +0545

    recipe detail enhanced

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,10 +1,37 @@
 const RecipeDetail = ({ data }) => {
     const { title, description, ingredients, image } = data[0];
-    return <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p>{ingredients}</p>
-    </div>;
+  return (
+    <>
+      {image ? (
+        <img
+          className=" mb-4 bg-cover w-full h-[250px] aspect-square object-cover opacity-70 "
+          src={image}
+          alt={title}
+        />
+      ) : (
+        ""
+      )}
+      <div className="  px-8 py-4">
+        <div className=" border-b-[1px] mb-4">
+          <h3 className=" font-bold text-2xl uppercase tracking-tight mb-2 ">
+            {title}
+          </h3>
+        </div>
+        <div className=" mb-4">
+          <h4 className=" font-semibold tracking-tight">Description</h4>
+          <p className=" ml-4 text-justify">{description}</p>
+        </div>
+        <div>
+          <h4 className=" font-semibold tracking-tight">Ingredients</h4>
+          <ol className="ml-4">
+            {ingredients?.map((element, index) => {
+              return <li key={index}>{element}</li>;
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RecipeDetail;
