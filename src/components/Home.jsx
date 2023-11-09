ec235734b079bd4641781fef425bd885cc299bde
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-09 17:11:30 +0545

    handled the delete and seperate add icon when there are data from local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,8 @@ const Home = () => {
 
   const handleDelete = (id) => {
     const updatedRecipeData = recipeData.filter((data) => data.id !== id);
-    setRecipeData(updatedRecipeData)
+    setRecipeData(updatedRecipeData);
+    setSelectedId(id - 1);
     localStorage.setItem("recipe", JSON.stringify(updatedRecipeData));
   };
 
@@ -36,71 +37,93 @@ const Home = () => {
   }, []);
 
   const handleTitleClick = (id) => {
-    console.log(`id: ${id} is clicked`);
     setSelectedId(id);
   };
 
   return (
     <div className="relative min-h-screen w-full grid place-items-center">
-      <div className=" flex gap-8 bg-red-100 w-10/12 min-h-[80vh] mt-12">
-        <div className=" bg-gray-200 w-4/12 px-8 py-4 min-h-full">
-          {recipeData
-            ? recipeData.map((recipe, index) => {
-                return (
-                  <div key={index}>
-                    <div
-                      className=" p-2 rounded hover:cursor-pointer hover:bg-gray-300 "
-                      onClick={() => handleTitleClick(recipe.id)}
-                    >
-                      <span className=" flex justify-between">
-                        <span>
-                          {"->"} {recipe.title}
+      {recipeData.length > 0 ? (
+        <div className=" flex gap-8 w-10/12 min-h-[80vh] mt-12">
+          <div className=" max-h-[80vh] overflow-x-auto w-4/12 px-8 py-4 min-h-full outline rounded outline-1 outline-gray-200">
+            <div className=" font-semibold tracking-tighter text-xl flex  justify-between items-center mb-4">
+              <h3>Recipe List</h3>
+              <AiOutlinePlus
+                onClick={handleAdd}
+                className=" hover:cursor-pointer text-gray-700 hover:rotate-180 transition-transform hover:text-gray-900 font-bold text-3xl"
+              />
+            </div>
+            {recipeData
+              ? recipeData.map((recipe, index) => {
+                  return (
+                    <div key={index}>
+                      <div
+                        className={`${
+                          selectedId === recipe.id && "bg-gray-200"
+                        } p-2 rounded hover:cursor-pointer hover:bg-gray-300 `}
+                        onClick={() => handleTitleClick(recipe.id)}
+                      >
+                        <span className=" flex justify-between">
+                          <span className=" uppercase">
+                            {"->"}{" "}
+                            {recipe.title && recipe.title.length > 30
+                              ? recipe.title.slice(0, 30) + "..."
+                              : recipe.title || "Untitled Recipe"}
+                          </span>
+                          <span
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              handleDelete(recipe.id);
+                            }}
+                            className=" hover:text-red-600"
+                          >
+                            <AiOutlineDelete className=" text-xl" />
+                          </span>
                         </span>
-                        <span
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleDelete(recipe.id);
-                          }}
-                          className=" hover:text-red-700"
-                        >
-                          <AiOutlineDelete />
-                        </span>
-                      </span>
+                      </div>
                     </div>
-                  </div>
-                );
-              })
-            : "no data found"}
-        </div>
-        <div className=" w-8/12 px-8 py-4">
-          {selectedId ? (
-            <>
+                  );
+                })
+              : "no data found"}
+          </div>
+          <div className=" w-8/12 max-h-[80vh] overflow-y-auto outline rounded outline-1 outline-gray-200">
+            {selectedId && (
               <RecipeDetail
                 data={recipeData.filter((obj) => obj.id === selectedId)}
               />
-            </>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-      <div className=" bg-red-100">
-        <div className="absolute bottom-20 right-20">
-          <div
-            onClick={handleAdd}
-            className="bg-blue-400 hover:bg-blue-500 hover:cursor-pointer transition-all inline-block p-5 rounded-full text-white"
-          >
-            <AiOutlinePlus className=" font-bold text-3xl" />
+            )}
           </div>
+          <AddRecipeModal
+            isOpen={isOpen}
+            onClose={handleClose}
+            onSave={handleSave}
+            recipeData={recipeData}
+            setRecipeData={setRecipeData}
+          />
         </div>
-        <AddRecipeModal
-          isOpen={isOpen}
-          onClose={handleClose}
-          onSave={handleSave}
-          recipeData={recipeData}
-          setRecipeData={setRecipeData}
-        />
-      </div>
+      ) : (
+        <div className=" ">
+          <div>
+            <h2 className=" tracking-tighter font-bold text-4xl text-gray-800">
+              Add Recipe
+            </h2>
+          </div>
+          <div className="absolute bottom-20 right-20">
+            <div
+              onClick={handleAdd}
+              className="bg-blue-400 hover:bg-blue-500 hover:cursor-pointer transition-all inline-block p-5 rounded-full text-white"
+            >
+              <AiOutlinePlus className=" font-bold text-3xl" />
+            </div>
+          </div>
+          <AddRecipeModal
+            isOpen={isOpen}
+            onClose={handleClose}
+            onSave={handleSave}
+            recipeData={recipeData}
+            setRecipeData={setRecipeData}
+          />
+        </div>
+      )}
     </div>
   );
 };
