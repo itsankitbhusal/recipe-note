732c9b92b2f5c91a0195cf8293e08df5e86211c6
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-10 18:25:18 +0545

    handle delete, edit added

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,16 +1,36 @@
-const RecipeDetail = ({ data }) => {
-    const { title, description, ingredients, image } = data[0];
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+
+const RecipeDetail = ({ data, handleDelete, handleEdit }) => {
+  let { title, description, ingredients, image, id } = data[0] ;
   return (
-    <>
-      {image ? (
-        <img
-          className=" mb-4 bg-cover w-full h-[250px] aspect-square object-cover opacity-70 "
-          src={image}
-          alt={title}
-        />
-      ) : (
-        ""
-      )}
+    <div>
+      <div className=" relative z-0 h-[300px] w-full">
+        <div className=" absolute z-10 inline-block right-0 p-2 rounded">
+          <div className=" flex gap-4">
+            <AiOutlineEdit
+              onClick={() => {
+                handleEdit(id);
+              }}
+              className=" text-2xl text-blue-700 hover:text-blue-900 hover:cursor-pointer"
+            />
+            <AiOutlineDelete
+              onClick={() => {
+                handleDelete(id);
+              }}
+              className=" text-2xl text-blue-700 hover:text-red-700 hover:cursor-pointer"
+            />
+          </div>
+        </div>
+        {image ? (
+          <img
+            className=" absolute mb-4 bg-cover w-full h-[250px] aspect-square object-cover opacity-70 "
+            src={image}
+            alt={title}
+          />
+        ) : (
+          ""
+        )}
+      </div>
       <div className="  px-8 py-4">
         <div className=" border-b-[1px] mb-4">
           <h3 className=" font-bold text-2xl uppercase tracking-tight mb-2 ">
@@ -30,7 +50,7 @@ const RecipeDetail = ({ data }) => {
           </ol>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
