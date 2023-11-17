aa7c5c062ecdb2af02f448460b24e236aad50bd2
itsankitbhusal <ankitbhusal95@gmail.com>
2023-11-17 22:46:19 +0545

    react hook form validation added

--- src/components/AddRecipeModal.jsx
+++ src/components/AddRecipeModal.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import ErrorMessage from "./ErrorMessage";
 
 const AddRecipeModal = ({
   isOpen,
@@ -9,6 +12,12 @@ const AddRecipeModal = ({
   updateData,
 }) => {
   const [recipe, setRecipe] = useState({});
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
     if (updateData) {
@@ -33,9 +42,8 @@ const AddRecipeModal = ({
     }
   };
 
-  const handleSave = (e) => {
-    e.preventDefault();
-    onSave(recipe);
+  const handleSave = (data) => {
+    onSave(data);
     setRecipe({});
     onClose();
   };
@@ -62,74 +70,118 @@ const AddRecipeModal = ({
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-gray-900"
-                  id="modal-headline"
-                >
-                  Add New Recipe
-                </h3>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    placeholder="Recipe Title"
-                    onChange={handleInputChange}
-                    value={recipe.title}
-                    className="mt-2 p-2 w-full border border-gray-300 rounded"
-                  />
-                  <textarea
-                    name="description"
-                    id="description"
-                    placeholder="Recipe Description"
-                    onChange={handleInputChange}
-                    value={recipe.description}
-                    className="mt-2 p-2 w-full border border-gray-300 rounded"
-                  />
-                  <input
-                    type="text"
-                    name="ingredients"
-                    id="ingredients"
-                    placeholder="Ingredients (comma-separated)"
-                    onChange={handleInputChange}
-                    value={recipe.ingredients}
-                    className="mt-2 p-2 w-full border border-gray-300 rounded"
-                  />
-                  <input
-                    type="text"
-                    name="image"
-                    id="image"
-                    placeholder="Image URL"
-                    onChange={handleInputChange}
-                    value={recipe.image}
-                    className="mt-2 p-2 w-full border border-gray-300 rounded"
-                  />
+          <form onSubmit={handleSubmit(handleSave)}>
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mt-3 w-full text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <h3
+                    className="text-lg leading-6 font-medium text-gray-900"
+                    id="modal-headline"
+                  >
+                    Add New Recipe
+                  </h3>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      placeholder="Recipe Title"
+                      onChange={handleInputChange}
+                      value={recipe.title}
+                      {...register("title", {
+                        required: true,
+                        minLength: 4,
+                        maxLength: 150,
+                      })}
+                      className="mt-2 p-2 w-full border border-gray-300 rounded"
+                    />
+                    {errors.title && errors.title.type === "required" && (
+                      <ErrorMessage message="Title is required!" />
+                    )}
+                    {errors.title && errors.title.type === "minLength" && (
+                      <ErrorMessage message="Minimum length id 4 chars!" />
+                    )}
+                    {errors.title && errors.title.type === "maxLength" && (
+                      <ErrorMessage message="Minimum length id 150 chars!" />
+                    )}
+                    <textarea
+                      name="description"
+                      id="description"
+                      placeholder="Recipe Description"
+                      onChange={handleInputChange}
+                      value={recipe.description}
+                      {...register("description", {
+                        required: true,
+                        minLength: 150,
+                        maxLength: 3000,
+                      })}
+                      className="mt-2 p-2 w-full border border-gray-300 rounded"
+                    />
+                    {errors.description &&
+                      errors.description.type === "required" && (
+                        <ErrorMessage message="Description is required!" />
+                      )}
+                    {errors.description &&
+                      errors.description.type === "minLength" && (
+                        <ErrorMessage message="Minimum length id 150 chars!" />
+                      )}
+                    {errors.description &&
+                      errors.description.type === "maxLength" && (
+                        <ErrorMessage message="Minimum length id 3000 chars!" />
+                      )}
+                    <input
+                      type="text"
+                      name="ingredients"
+                      id="ingredients"
+                      placeholder="Ingredients (comma-separated)"
+                      onChange={handleInputChange}
+                      value={recipe.ingredients}
+                      {...register("ingredients", {
+                        required: true,
+                        minLength: 4,
+                      })}
+                      className="mt-2 p-2 w-full border border-gray-300 rounded"
+                    />
+                    {errors.ingredients &&
+                      errors.ingredients.type === "required" && (
+                        <ErrorMessage message="Ingredients is required!" />
+                      )}
+                    {errors.ingredients &&
+                      errors.ingredients.type === "minLength" && (
+                        <ErrorMessage message="Minimum length id 4 chars!" />
+                      )}
+                    <input
+                      type="text"
+                      name="image"
+                      id="image"
+                      placeholder="Image URL"
+                      onChange={handleInputChange}
+                      value={recipe.image}
+                      className="mt-2 p-2 w-full border border-gray-300 rounded"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              onClick={handleSave}
-              type="button"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Save
-            </button>
-            <button
-              onClick={() => {
-                setRecipe({});
-                onClose()
-              }}
-              type="button"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-            >
-              Cancel
-            </button>
-          </div>
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="submit"
+                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Save
+              </button>
+              <button
+                onClick={() => {
+                  setRecipe({});
+                  onClose();
+                }}
+                type="button"
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
